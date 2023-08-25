0bc3de14e63ab636440131b482601366b3df8cb5
Benjamin De-graft-Andoh <degraftandohbenjamin@gmail.com>
2023-08-25 06:59:30 +0000

    PiniaClasswork.vue

---
+++ Vue-todoclasswork/src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import InputComponent from "../components/InputComponent.vue";
+
+</script>
+
+<template>
+  <main>
+    
+    <InputComponent />
+    <div class="textBox">
+      <div class="txt">
+        <h1>Manage your tasks effectively</h1>
+        <p>Modern todo list application to 
+          help you track your activities and stay productive.
+        </p>
+        <button><RouterLink to="/task">Get Started</RouterLink></button>
+      </div>
+      <img src="../assets/hero-illustration.png"/>
+    </div>
+  
+  </main>
+</template>
+
+<style scope>
+
+.textBox{
+  display: flex;
+  padding-top: 50px;
+  gap: 50px;
+  padding: 27px 370px 0px 370px;
+}
+.txt{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 20px;
+}
+</style>
